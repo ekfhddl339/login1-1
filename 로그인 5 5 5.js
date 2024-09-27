8214c4135d3f9e67e8131a2a6be95c7c2de2f402
Daisy306dev <developinglhs@naver.com>
2024-09-27 10:08:34 +0900

    Delete 로그인 5 5 5.js

--- 로그인 5 5 5.js
+++
@@ -1,8 +0,0 @@
-function login() {
-    const el = document.getElementById("ID").value;
-    const welcomeMessage = document.getElementById("welcomeMessage");
-    welcomeMessage.innerHTML = "Welcome to 'Naevis' world' mrs/ms. " + el + " !";
-    document.getElementById("naevisDoneMovie").setAttribute("href", 
-        "https://www.youtube.com/watch?v=2ZdVzQRkS4c");
-    /* a의 element에 속성을 부여해주기...*/
-}
